5a049a0fd87017fc35084f561727dcecbf04c0fc
sandromarceddu <152404263+sandromarceddu@users.noreply.github.com>
2025-10-30 19:19:56 +0100

    Add files via upload

---
+++ app/LanguageContext.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("it");
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
