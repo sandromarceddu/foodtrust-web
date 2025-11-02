94e943bb7f34a95c745d0189dbbc351279490055
sandromarceddu <152404263+sandromarceddu@users.noreply.github.com>
2025-11-02 19:20:36 +0100

    Update page.jsx

--- app/prodotti/page.jsx
+++ app/prodotti/page.jsx
@@ -1,8 +1,7 @@
-
-export default function Page(){
+export default function Page() {
   return (
-    <main style={padding:'40px'}>
-      <h1>Prodotti page</h1>
+    <main style={{ padding: "40px" }}>
+      <h1>Prodotti</h1>
       <p>Contenuto della pagina prodotti.</p>
     </main>
   );
