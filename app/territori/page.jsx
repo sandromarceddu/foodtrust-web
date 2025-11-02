09481deb5b6eef1b23da7f68820b2a122d992485
sandromarceddu <152404263+sandromarceddu@users.noreply.github.com>
2025-11-02 19:20:58 +0100

    Update page.jsx

--- app/territori/page.jsx
+++ app/territori/page.jsx
@@ -1,8 +1,7 @@
-
-export default function Page(){
+export default function Page() {
   return (
-    <main style={padding:'40px'}>
-      <h1>Territori page</h1>
+    <main style={{ padding: "40px" }}>
+      <h1>Territori</h1>
       <p>Contenuto della pagina territori.</p>
     </main>
   );
